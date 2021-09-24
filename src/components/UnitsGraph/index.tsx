f76f92cf2756190b459507a988d1070187a2d7ae
Davidxy9 <davidwilliamlima@hotmail.com>
2021-09-24 15:41:05 -0300

    First unity

--- src/components/UnitsGraph/index.tsx
+++ src/components/UnitsGraph/index.tsx
@@ -2,9 +2,10 @@ import Highcharts from 'highcharts';
 import HighchartsReact from 'highcharts-react-official';
 import { useState, useEffect } from 'react';
 import api from '../../services/api';
+import {configUnitsGraph} from '../../utils/configUnitsGraph';
 
 interface UnitsData {
-    id: number;
+    //id: number;
     name: string;
 }
 
@@ -12,13 +13,15 @@ interface UnitsData {
 export function UnitsGraph() {
     const [units, setUnits] = useState<UnitsData[]>([]);
 
-    useEffect(() => {},[
-        api.get(`https://my-json-server.typicode.com/tractian/fake-api/units`)
-        .then(response => setUnits(response.data))
-    ])
+    useEffect(() => {
+        
+         api.get(`https://my-json-server.typicode.com/tractian/fake-api/units/1`)
+            .then(response => setUnits(response.data.name)) 
+            
+    
+    }, [])
  
-
-
+    console.log(units)
     const configUnitsGraph = {
         chart: {
             type: 'column'
@@ -67,24 +70,23 @@ export function UnitsGraph() {
             }
         },
         series: [{
-            name: 'Tokyo',
+            name: units,
             data: [49.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
-
+    
         }, {
             name: 'New York',
             data: [83.6, 78.8, 98.5, 93.4, 106.0, 84.5, 105.0, 104.3, 91.2, 83.5, 106.6, 92.3]
-
+    
         }, {
             name: 'London',
             data: [48.9, 38.8, 39.3, 41.4, 47.0, 48.3, 59.0, 59.6, 52.4, 65.2, 59.3, 51.2]
-
+    
         }, {
             name: 'Berlin',
             data: [42.4, 33.2, 34.5, 39.7, 52.6, 75.5, 57.4, 60.4, 47.6, 39.1, 46.8, 51.1]
-
+    
         }]
     }
-    
     return (
         <HighchartsReact
             highcharts={Highcharts}
