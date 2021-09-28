2d5820a8789d73edac7fe344917f309eb60b43bb
Davidxy9 <davidwilliamlima@hotmail.com>
2021-09-27 22:17:54 -0300

    im back

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,11 @@
 import {AppProps} from 'next/app';
 import { Header } from '../components/Header';
 import { GlobalStyle } from '../styles/global';
-import withNProgress from 'next-nprogress';
-import NprogressStyles from 'next-nprogress/styles'
+
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-    <NprogressStyles color="#e34234" spinner={false} />
     <GlobalStyle />
     <Header />
     <Component {...pageProps} />
@@ -15,6 +13,4 @@ function MyApp({ Component, pageProps }: AppProps) {
   )
 }
 
-const msDelay = 200;
-const configOptions = { trickleSpeed: 50 }; 
-export default withNProgress(msDelay, configOptions)(MyApp);
+export default MyApp
