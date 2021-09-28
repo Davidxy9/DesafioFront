8f1f922c179770a5735429032549b821ef069b41
Davidxy9 <davidwilliamlima@hotmail.com>
2021-09-27 21:47:12 -0300

    loading

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,13 @@
 import {AppProps} from 'next/app';
 import { Header } from '../components/Header';
 import { GlobalStyle } from '../styles/global';
+import NextProgress from "next-nprogress";
 
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+    <NextProgress delay={300} options={{ showSpinner: false }} />
     <GlobalStyle />
     <Header />
     <Component {...pageProps} />
